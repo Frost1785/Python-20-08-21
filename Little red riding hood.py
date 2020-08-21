18d2fefb14f2c0b86ce4f61a947ec3acc5e74fe4
Frost <69790374+Frost1785@users.noreply.github.com>
2020-08-21 09:17:57 +0800

    Add files via upload

---
+++ Little red riding hood.py
@@ -0,0 +1,23 @@
+name = input("What's your name:")
+cname = input("What do you want to name your game console:")
+
+print ("Hi" ,name,"!" "Welcome to", cname, "!")
+print("\033[2;37;40m HI! I AM YOUR GAME CONSOLE", cname,"THERE IS AN URGENT MATTER WE NEED TO DISCUSS! \n")
+a = input("ARE YOU SURE YOU WANT TO CONTINUE? THIS MAY BE A DANGEROUS JOURNEY(TYPE YES IF YOU DO AND NO IF YOU DON'T):")
+if a == 'yes':
+    print ("PLAYER ONE, GET READY! THIS IS GOING TO BE ONE HECK OF A RIDE!")
+    print ("HERE IS A LIST OF ALL THE DIFFERENT GAMEMODES! PLEASE CHOOSE ONE.")
+    print (")
+    
+    
+
+
+
+
+
+
+
+
+else:
+    print ("THIS IS A GREAT GAME AND YOU WILL REGRET IT! BYE.")
+
